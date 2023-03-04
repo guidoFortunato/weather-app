cd3e72bd7f6eb7260dd418213e31ea3eea645b18
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-03-04 20:34:11 -0300

    comienza proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      a
+    </div>
+  )
+}
+
+export default App
